ab259ad1dd94ae6adf0ac9b4750bcc60df5f1ba0
lyndoncortez <lyndoncortez@yahoo.com>
2021-05-29 10:16:36 +0800

    Initial commit

---
+++ blog_project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-f40k67f2esndtqzc@bpnj=&*978=0lw3(rc(w$!*nt4e86yl%@'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
